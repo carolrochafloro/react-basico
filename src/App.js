c10340fd0f34a0a6882e7f345511c22e26abf1c0
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-05 14:05:34 -0300

    Stateful component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import Comment from './components/Comment';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu projeto</h1>
-      <Comment name="Carol">Mensagem.</Comment>
-    </div>
-  );
+class App extends Component {
+  state = {
+    comments: [
+      {
+        name: 'Carol',
+        message: 'Teste',
+      },
+      {
+        name: 'Gustavo',
+        message: 'Outro teste',
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Meu projeto</h1>
+
+        {this.state.comments.map((comment, index) => (
+          <Comment key={index} name={comment.name}>
+            {comment.message}
+          </Comment>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
