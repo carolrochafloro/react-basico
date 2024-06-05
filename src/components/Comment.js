dc6a923a24cc763b00b40f5deb3d844d3b92538a
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-05 13:37:49 -0300

    1st componnent

---
+++ src/components/Comment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Comment.css';
+
+const Comment = () => (
+  <div className="Comment">
+    <div>Carol:</div>
+    <div>Um comentário</div>
+  </div>
+);
+
+export default Comment;
